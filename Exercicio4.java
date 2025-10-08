66920c715c126e8292b8a777244f73ac32876fd5
Matheus Rodrigues Alexandre e Silva <matheus4rodrigues@hotmail.com>
2025-10-08 20:20:55 -0300

    Update Exercicio4.java

--- Exercicio4.java
+++ Exercicio4.java
@@ -1,8 +1,8 @@
-import java.util.Scanner;
 /*4. Conversão de Celsius para Fahrenheit
 Converta a temperatura com a fórmula:
 F = (C × 9/5) + 32
 */
+import java.util.Scanner;
 class Exercicio4
 {
     public static void main(String[] args) 
