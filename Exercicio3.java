6796d30f9b408ac7aa66973812c006c8095381dc
Matheus Rodrigues Alexandre e Silva <matheus4rodrigues@hotmail.com>
2025-10-08 20:05:41 -0300

    Create Exercicio3.java

---
+++ Exercicio3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+/*3. Cálculo de Idade
+Peça ao usuário o ano de nascimento, calcule a idade com base no ano atual (ex: 2025) e
+exiba o resultado no console.*/
+class Exercicio3
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o seu ano de nascimento: ");
+        int anoNascimento = sc.nextInt();
+        int idade = 2025 - anoNascimento;
+        System.out.println("A sua idade é: " + idade);
+    }
+}
