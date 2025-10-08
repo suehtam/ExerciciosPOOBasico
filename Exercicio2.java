fa76fdfcce8797a6870823dd930889caaa58d8e6
Matheus Rodrigues Alexandre e Silva <matheus4rodrigues@hotmail.com>
2025-10-08 19:59:17 -0300

    Create Exercicio2.java

---
+++ Exercicio2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+/*2. Média de Três Números
+Solicite três números e imprima a média aritmética.*/
+class Exercicio2
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um número: ");
+        int numero1 = sc.nextInt();
+        System.out.println("Digite um número: ");
+        int numero2 = sc.nextInt();
+        System.out.println("Digite um número: ");
+        int numero3 = sc.nextInt();
+        double media = (numero1 + numero2 + numero3) / 3;
+        System.out.println("A média aritmética dos números é: " + media);
+    }
+}
