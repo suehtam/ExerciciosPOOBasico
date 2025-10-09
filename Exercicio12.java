72760f60d52477fac5414b085381cc78d7fff297
Matheus Rodrigues Alexandre e Silva <matheus4rodrigues@hotmail.com>
2025-10-09 13:32:06 -0300

    Update Exercicio12.java

--- Exercicio12.java
+++ Exercicio12.java
@@ -1,11 +1,11 @@
 /*
-12. Teorema de Pitágoras 
-Peça os valores dos catetos a e b, e calcule a hipotenusa: 
-hipotenusa = √(a² + b²) 
-Use Math.sqrt() e System.out.printf() com 2 casas decimais. 
+13. Volume de uma Esfera 
+Peça o raio (entre 1.0 e 50.0) e calcule o volume: 
+V = (4/3) × π × r³ 
+Use π = 3.14 e arredonde com 2 casas decimais.
 */
 import java.util.Scanner;
-class Exercicio12
+class Exercicio13
 {
     public static void main(String[] args) 
     {
@@ -13,7 +13,7 @@ class Exercicio12
         System.out.println("Digite o valor do cateto A ");
         int catetoA = sc.nextInt();
         System.out.println("Digite o valor do cateto B ");
-        int catetoB = isc.nextInt();
+        int catetoB = sc.nextInt();
         double hipotenusa = Math.sqrt(Math.pow(catetoA, 2) + Math.pow(catetoB, 2));
         System.out.printf("O valor da hipotenusa é: %.2f", hipotenusa);
     }
